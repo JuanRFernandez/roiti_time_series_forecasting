e701e985e69b7e9e1b316aec763cb746fe7aab25
JuanRFernandez <roferbru@gmail.com>
2025-11-09 17:01:30 +0100

    Initial commit: Day ahead forecast project structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='this is a project to solve a case study',
+    author='Juan R Fernandez',
+    license='MIT',
+)
